483aa822f416cf7ac1e6ad8c559caeaeca752440
HaydenJones1125 <haydenj1125@yahoo.com>
2025-11-16 09:21:59 -0600

    Updated Week Number

--- index.js
+++ index.js
@@ -1089,7 +1089,7 @@ const cron = require('node-cron');
 const fetch = require('node-fetch');
 
 let year = new Date().getFullYear();
-let currentFootballWeekNumber = 12;
+let currentFootballWeekNumber = 13;
 
 /*
     Functionality to change the year on July 1st and delete database entries for tblPicksLeft, tblSelections, tblGroupMembers, and then tblGroups
